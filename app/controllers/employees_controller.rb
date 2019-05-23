9d4ea2c36a6945d37e913da7731f35f34e0612ef
rmaghsoudi <romy_mag@outlook.com>
2019-05-23 11:39:02 -0400

    Basic functionality

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,45 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        @dog = @employee.dog
+    end
+
+    def new
+        @employee = Employee.new
+    end
+
+    def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+    end
+
+    def update
+        @employee = Employee.find(params[:id])
+        if @employee.update(employee_params)
+            redirect_to employee_path(@employee)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name,
+        :alias, :title, :office, :img_url, :dog_id)
+    end
+
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,5 @@
 class Employee < ApplicationRecord
+    validates :alias, uniqueness: true
+    validates :title, uniqueness: true
+    belongs_to :dog
 end
